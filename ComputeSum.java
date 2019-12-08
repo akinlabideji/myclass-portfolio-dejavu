6e032c2f33fed005257c52c36e9cb14d4dbacab0
akinlabideji <52798383+akinlabideji@users.noreply.github.com>
2019-12-07 21:51:08 -0500

    Create ComputeSum.java
    Sum of two numbers

---
+++ ComputeSum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ComputeSum {
+public static void main(String[] args) {
+Scanner input= new Scanner(System.in);
+System.out.print("Enter two integer numbers: ");
+int number1 =input.nextInt();
+int number2 =input.nextInt();
+System.out.println("The sum of " + number1 * number1 + " " + number2 * number2);
+ }
+}
