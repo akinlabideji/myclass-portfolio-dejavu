0f23dc78f4bace62726243a4d1a42f90f08b8c92
akinlabideji <52798383+akinlabideji@users.noreply.github.com>
2020-02-13 16:18:01 -0500

    Create TestBoolean.java
    Testing boolean

---
+++ TestBoolean.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class TestBooleanOperator {
+public static void main(String[] args) {
+
+//Create a scanner
+Scanner input = new Scanner(System.in);
+System.out.println(
