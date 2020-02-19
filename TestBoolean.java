82d47bae9df2a452a44e888f1b552307e44322e7
akinlabideji <52798383+akinlabideji@users.noreply.github.com>
2020-02-18 20:22:04 -0500

    Update TestBoolean.java

--- TestBoolean.java
+++ TestBoolean.java
@@ -4,4 +4,13 @@ public static void main(String[] args) {
 
 //Create a scanner
 Scanner input = new Scanner(System.in);
-System.out.println(
+  
+//Receive an input
+System.out.print(("Enter an integer: ");
+int number = input.nextInt();
+                 
+System.out.println("Is " + number + " divisible by 2 and 3? " +
+                   ((number % 2 == 0) && (number % 3 == 0)));
+                 
+System.out.println("Is " + number + " divisible by 2 or 3? " +
+                   ((number % 2 == 0) || (number % 3 == 0)));
